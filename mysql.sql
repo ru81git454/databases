b329e613da6556be51b62a57154f9840994c3473
ru81git454 <ru81o454@gmail.com>
2018-10-09 10:15:45 +0800

    index.php

--- mysql.sql
+++ mysql.sql
@@ -1,7 +1,9 @@
 #commend
-MySQL -u root -p
+#mySQL -u root -p
 Status
 show databases;
+
+
 create database g106078701_db;
 use g106078701_db;
 
@@ -10,3 +12,4 @@ Select user,host from user;
 Grant all on test.* to g106078701@localhost identified by 'Ggau3vup@' with options;
 
 set password for root@localhost=password('Ggau3vup@')
+#ggau3vup 
